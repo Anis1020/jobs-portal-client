3a73a8d813bfea66b8496065f2f7f005195acd60
nexgensoft0 <nexgensoft0@gmail.com>
2025-01-29 18:28:33 +0600

    project setup done

---
+++ src/Forms/Registration/Registration.jsx
@@ -0,0 +1,9 @@
+const Registration = () => {
+  return (
+    <div>
+      <h2>register form coming soon</h2>
+    </div>
+  );
+};
+
+export default Registration;
---
+++ src/Pages/Home/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h2>home content</h2>
+    </div>
+  );
+};
+
+export default HomePage;
